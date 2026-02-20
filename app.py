43f4e5364973d0a3442e824d0f9cf38e05a3e16c
Yakshanyai_M <myakshanyai@gmail.com>
2026-02-20 18:51:09 +0530

    Changes are made in allow_credentials

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Later restrict to your framer domain
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
